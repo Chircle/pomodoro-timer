d4e2cbdec5bf5efd9bbe58753a379b82eb59b0b3
Simert,Mechu <Mechu.Simert@adesso.de>
2026-03-18 15:20:08 +0100

    base path set also for favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import './globals.css'
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
+const base = process.env.NEXT_PUBLIC_BASE_PATH ?? ''
+
 export const metadata: Metadata = {
   title: 'Pomodoro Timer – Focus & Productivity App',
   description: 'A beautiful Pomodoro timer to boost your focus and productivity. Customize work intervals, breaks, and rounds. Stay on track with ambient music and visual cues.',
@@ -21,33 +23,33 @@ export const metadata: Metadata = {
     title: 'Pomodoro Timer – Focus & Productivity App',
     description: 'A beautiful Pomodoro timer to boost your focus and productivity.',
   },
-  manifest: '../favicon/site.webmanifest',
+  manifest: `${base}/favicon/site.webmanifest`,
   icons: {
     icon: [
       {
-        url: '../favicon/favicon-16x16.png',
+        url: `${base}/favicon/favicon-16x16.png`,
         sizes: '16x16',
         type: 'image/png',
       },
       {
-        url: '../favicon/favicon-32x32.png',
+        url: `${base}/favicon/favicon-32x32.png`,
         sizes: '32x32',
         type: 'image/png',
       },
       {
-        url: '../favicon/favicon.ico',
+        url: `${base}/favicon/favicon.ico`,
         sizes: 'any',
       },
     ],
-    apple: '../favicon/apple-touch-icon.png',
+    apple: `${base}/favicon/apple-touch-icon.png`,
     other: [
       {
         rel: 'android-chrome-192x192',
-        url: '../favicon/android-chrome-192x192.png',
+        url: `${base}/favicon/android-chrome-192x192.png`,
       },
       {
         rel: 'android-chrome-512x512',
-        url: '../favicon/android-chrome-512x512.png',
+        url: `${base}/favicon/android-chrome-512x512.png`,
       },
     ],
   },
